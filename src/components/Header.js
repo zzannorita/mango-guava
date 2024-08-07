5f1f7768c4e8e2c88fe26be35179f42ffcd971e2
zzannorita <tpals0518@naver.com>
2024-08-07 16:21:09 +0900

    Design: 헤더 페이지 css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,57 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import HeaderStyle from "../styles/header.module.css";
+import logo from "../image/logo.png";
+import alarmOff from "../image/alarmOff.png";
+import chat from "../image/chat.png";
+import person from "../image/person.png";
+import sell from "../image/sell.png";
+import SearchBox from "../components/SearchBox";
+
 function Header() {
   return (
-    <header>
-      헤더입니다
-      <nav>
-        <Link to="/">홈페이지</Link>
-        <Link to="/regist">판매 페이지</Link>
-        <Link to="/chat">채팅 페이지</Link>
-        <Link to="/profile">마이 페이지</Link>
-      </nav>
+    <header className="container">
+      <div className={HeaderStyle.headerBox}>
+        <div className={HeaderStyle.headerTop}>
+          <div className={HeaderStyle.headerTopRightBox}>
+            <div className={HeaderStyle.loginBox}>
+              <Link className={HeaderStyle.link} to="/login">
+                로그인/회원가입
+              </Link>
+            </div>
+            <div className="commonStyle">
+              <img
+                className={HeaderStyle.alarmImg}
+                alt="alarmOff"
+                src={alarmOff}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={HeaderStyle.headerBottom}>
+          <div className={HeaderStyle.logoBox}>
+            <div className={HeaderStyle.logo}>
+              <img alt="logo" src={logo} />
+            </div>
+            <div>망고와 구아바</div>
+          </div>
+          <SearchBox />
+          <div className={HeaderStyle.rightBox}>
+            <div className={HeaderStyle.box}>
+              <img className={HeaderStyle.imgSize} alt="sell" src={sell} />
+              <div className={HeaderStyle.overlay}>판매</div>
+            </div>
+            <div className={HeaderStyle.box}>
+              <img className={HeaderStyle.imgSize} alt="chat" src={chat} />
+              <div className={HeaderStyle.overlay}>채팅</div>
+            </div>
+            <div className={HeaderStyle.box}>
+              <img className={HeaderStyle.imgSize} alt="perosn" src={person} />
+              <div className={HeaderStyle.overlay}>마이</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
