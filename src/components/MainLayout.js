7eacef36b7d1c9e09533dbd3c54e1dfccfe0a283
zzannorita <tpals0518@naver.com>
2024-08-08 02:21:43 +0900

    Design: 최근본상품 css 적용

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,6 +2,8 @@ import React from "react";
 import Category from "./Category";
 import RealTime from "./RealTime";
 import MainLayoutStyle from "../styles/mainLayout.module.css";
+import exImg from "../image/exImg.png";
+import eyeImg from "../image/eye.png";
 
 export default function MainLayout() {
   return (
@@ -9,6 +11,39 @@ export default function MainLayout() {
       <div className={MainLayoutStyle.container}>
         <Category />
         <RealTime />
+        {/* 최근본상품 */}
+        <div className={MainLayoutStyle.recentBoxContainer}>
+          <div className={MainLayoutStyle.recentBoxTop}>
+            <div>Today</div>
+            <img
+              className={MainLayoutStyle.eyeImg}
+              src={eyeImg}
+              alt="eyeImg"
+            ></img>
+          </div>
+          <div className={MainLayoutStyle.recentImgBox}>
+            <img
+              className={MainLayoutStyle.imageImg}
+              src={exImg}
+              alt="imageImg"
+            ></img>
+            <img
+              className={MainLayoutStyle.imageImg}
+              src={exImg}
+              alt="imageImg"
+            ></img>
+            <img
+              className={MainLayoutStyle.imageImg}
+              src={exImg}
+              alt="imageImg"
+            ></img>
+            <img
+              className={MainLayoutStyle.imageImg}
+              src={exImg}
+              alt="imageImg"
+            ></img>
+          </div>
+        </div>
       </div>
     </div>
   );
