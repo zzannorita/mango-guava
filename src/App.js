05b234ae383475b55d52259b75420568ab359f1d
zzannorita <tpals0518@naver.com>
2024-08-07 18:46:40 +0900

    Design: 라우터에 메인레이아웃 추가 및 경계선 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import MainLayout from "./components/MainLayout";
 import Home from "./pages/Home";
 import Regist from "./pages/Regist";
 import Profile from "./pages/Profile";
@@ -14,15 +15,19 @@ function App() {
   return (
     <Router>
       <Header />
+      <hr className="separator" />
+      <MainLayout />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/regist" element={<Regist />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/search" element={<Search />} />
+        {/* <Route path="/products?item={search-data}" element={<Search />} /> */}
         <Route path="/chat" element={<Chat />} />
         <Route path="/login" element={<Login />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/detail" element={<Detail />} />
+        {/* <Route path="/detail?itemId={productId}" element={<Detail />} /> */}
       </Routes>
     </Router>
   );
