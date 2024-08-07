ecd370456ceb0aa90245439ae6574dba4487c762
zzannorita <tpals0518@naver.com>
2024-08-07 18:45:36 +0900

    Design: 카테고리 css

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,26 +2,35 @@ import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import CategoryStyle from "../styles/category.module.css";
 import categoryImg from "../image/category.png";
+import greaterImg from "../image/greater.png";
 
 export default function Category() {
   ///////////////////카테고리 hover시////////////////////
   const [hoveredCategory, setHoveredCategory] = useState(null);
   const [hoveredSubCategory, setHoveredSubCategory] = useState(null);
+  const [hoveredSubSubCategory, setHoveredSubSubCategory] = useState(null);
 
-  //첫번째 카테고리 하버시
+  //첫번째 카테고리 hover시
   const handleMouseHover = (category) => {
     setHoveredCategory(category);
+    setHoveredSubSubCategory(null);
   };
 
   //카테고리 중복 뜸 방지
   const handleMouseLeave = () => {
     setHoveredCategory(null);
     setHoveredSubCategory(null);
+    setHoveredSubSubCategory(null);
   };
 
-  //두번째 카테고리 하버시
+  //두번째 카테고리 hover시
   const handleSubMouseHover = (subCategory) => {
     setHoveredSubCategory(subCategory);
+    setHoveredSubSubCategory(null);
+  };
+
+  const handleSubSubMouseHover = (subSubCategory) => {
+    setHoveredSubSubCategory(subSubCategory);
   };
 
   return (
@@ -31,31 +40,79 @@ export default function Category() {
         alt="categoryImg"
         src={categoryImg}
       />
+      <div className={CategoryStyle.selectedCategoryBox}>
+        <div
+          className={`${CategoryStyle.selectedCategory} ${
+            CategoryStyle.selectedCategory1
+          } ${hoveredCategory ? CategoryStyle.visible : ""}`}
+        >
+          {hoveredCategory || ""}
+        </div>
+        <img
+          className={`${CategoryStyle.greaterImg} ${
+            CategoryStyle.greaterImg1
+          } ${hoveredSubCategory ? CategoryStyle.visible : ""}`}
+          alt="greaterImg"
+          src={greaterImg}
+        />
+        <div
+          className={`${CategoryStyle.selectedCategory} ${
+            CategoryStyle.selectedCategory2
+          } ${hoveredSubCategory ? CategoryStyle.visible : ""}`}
+        >
+          {hoveredSubCategory || ""}
+        </div>
+        <img
+          className={`${CategoryStyle.greaterImg} ${
+            CategoryStyle.greaterImg2
+          } ${hoveredSubSubCategory ? CategoryStyle.visible : ""}`}
+          alt="greaterImg"
+          src={greaterImg}
+        />
+        <div
+          className={`${CategoryStyle.selectedCategory} ${
+            CategoryStyle.selectedCategory3
+          } ${hoveredSubSubCategory ? CategoryStyle.visible : ""}`}
+        >
+          {hoveredSubSubCategory || ""}
+        </div>
+      </div>
       <div className={CategoryStyle.navbar}>
         <ul>
           <li
-            onMouseEnter={() => handleMouseHover("clothing")}
+            onMouseEnter={() => handleMouseHover("의류")}
             onMouseLeave={handleMouseLeave}
           >
             <Link to="/clothing">의류</Link>
-            {hoveredCategory === "clothing" && (
+            {hoveredCategory === "의류" && (
               <div className={CategoryStyle.subNavbar}>
                 <ul>
                   <li
-                    onMouseEnter={() => handleSubMouseHover("men")}
+                    onMouseEnter={() => handleSubMouseHover("남성의류")}
                     onMouseLeave={handleMouseLeave}
                   >
                     <Link to="/clothing/men">남성의류</Link>
-                    {hoveredSubCategory === "men" && (
+                    {hoveredSubCategory === "남성의류" && (
                       <div className={CategoryStyle.subSubNavbar}>
                         <ul>
-                          <li>
+                          <li
+                            onMouseEnter={() => handleSubSubMouseHover("상의")}
+                            onMouseLeave={handleMouseLeave}
+                          >
                             <Link to="/clothing/men/shirts">상의</Link>
                           </li>
-                          <li>
+                          <li
+                            onMouseEnter={() => handleSubSubMouseHover("하의")}
+                            onMouseLeave={handleMouseLeave}
+                          >
                             <Link to="/clothing/men/pants">하의</Link>
                           </li>
-                          <li>
+                          <li
+                            onMouseEnter={() =>
+                              handleSubSubMouseHover("운동화")
+                            }
+                            onMouseLeave={handleMouseLeave}
+                          >
                             <Link to="/clothing/men/shoes">운동화</Link>
                           </li>
                         </ul>
@@ -73,13 +130,13 @@ export default function Category() {
             )}
           </li>
           <li>
-            <Link to="/link2">가전제품</Link>
+            <Link to="/electronic">가전제품</Link>
           </li>
           <li>
-            <Link to="/link3">주방용품</Link>
+            <Link to="/kitchen">주방용품</Link>
           </li>
           <li>
-            <Link to="/link4">자동차용품</Link>
+            <Link to="/car">자동차용품</Link>
           </li>
         </ul>
       </div>
